acef2f2eaeb969ca782040d5e9edd15998caae33
renghen <renghen@yahoo.com>
2020-01-30 21:11:51 +0400

    account construction refactored

--- src/main/kotlin/com/revolut/accounts/Account.kt
+++ src/main/kotlin/com/revolut/accounts/Account.kt
@@ -7,11 +7,19 @@ import org.multiverse.api.references.TxnLong
 class NotEnoughMoneyException : Exception("Not enough money")
 
 
-class Account(accountNumber: String, initialBalance: Double) {
+class Account private constructor (accountNumber: String, initialBalance: Double) {
+
+    companion object{
+          fun createAccount(amount:Double) : Account{
+              return Account("",amount)
+          }
+    }
+
     private val accountNumber = accountNumber
     private val lastUpdate: TxnLong = StmUtils.newTxnLong(System.currentTimeMillis())
     private val balance: TxnDouble = StmUtils.newTxnDouble(initialBalance)
 
+    @Throws(IllegalArgumentException::class)
     fun addMoney(amount: Double): Double {
         if (amount < 0) {
             throw IllegalArgumentException("Wrong amount");
@@ -24,6 +32,12 @@ class Account(accountNumber: String, initialBalance: Double) {
         return balance.atomicGet()
     }
 
+    @Throws(IllegalArgumentException::class)
+    operator fun plus(amount: Double): Double {
+        return addMoney(amount)
+    }
+
+    @Throws(IllegalArgumentException::class, NotEnoughMoneyException::class)
     fun removeMoney(amount: Double): Double {
         if (amount < 0) {
             throw IllegalArgumentException("Wrong amount")
@@ -37,6 +51,11 @@ class Account(accountNumber: String, initialBalance: Double) {
             lastUpdate.set(System.currentTimeMillis())
         })
         return balance.atomicGet()
+    }
+
+    @Throws(IllegalArgumentException::class, NotEnoughMoneyException::class)
+    operator fun minus(amount: Double): Double {
+        return removeMoney(amount)
     }
 
     fun transferTo(other: Account, amount: Double) {

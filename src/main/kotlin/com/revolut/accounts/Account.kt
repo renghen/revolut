c69bbce6091d254c6355aa54130e9dbda9efe248
renghen <renghen@yahoo.com>
2020-01-30 19:54:14 +0400

    account structure added

---
+++ src/main/kotlin/com/revolut/accounts/Account.kt
@@ -0,0 +1,48 @@
+package com.revolut.accounts
+
+import org.multiverse.api.StmUtils
+import org.multiverse.api.references.TxnDouble
+import org.multiverse.api.references.TxnLong
+
+
+
+
+
+class Account(accountNumber: String, initialBalance: Double) {
+    private val accountNumber = accountNumber
+    private val lastUpdate: TxnLong = StmUtils.newTxnLong(System.currentTimeMillis())
+    private val balance: TxnDouble = StmUtils.newTxnDouble(initialBalance)
+
+    fun addMoney(amount: Double): Double {
+        if (amount < 0) {
+            throw IllegalArgumentException("Wrong amount");
+        }
+
+        StmUtils.atomic(Runnable {
+            balance.atomicIncrementAndGet(amount)
+            lastUpdate.set(System.currentTimeMillis())
+        })
+        return balance.atomicGet()
+    }
+
+    fun removeMoney(amount: Double) : Double{
+        if (amount < 0) {
+            throw IllegalArgumentException("Wrong amount");
+        }
+        StmUtils.atomic(Runnable {
+            balance.atomicIncrementAndGet(-1*amount)
+            lastUpdate.set(System.currentTimeMillis())
+        })
+        return balance.atomicGet()
+    }
+
+//    fun adjustBy(amount: Int, date: Long) {
+//        StmUtils.atomic(Runnable {
+//            balance.increment(amount)
+//            lastUpdate.set(date)
+//            require(balance.get() > 0) { "Not enough money" }
+//        })
+//    }
+
+
+}

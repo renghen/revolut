b2614b84ced9b1197ed96d1268911fdbe2f0df09
renghen <renghen@yahoo.com>
2020-01-31 22:17:15 +0400

    test for add and remove money

--- src/test/kotlin/com/revolut/accounts/models/AccountTest.kt
+++ src/test/kotlin/com/revolut/accounts/models/AccountTest.kt
@@ -13,6 +13,46 @@ class AccountTest {
     }
 
     @Test
+    fun `add incorrect amount`() {
+        val account = bank["0005"]
+        if (account != null) {
+            assertFailsWith(IllegalArgumentException::class) {
+                account.addMoney(-1.0)
+            }
+        } else {
+            assertNotNull(account, "account '0005' cannot be null")
+        }
+    }
+
+    @Test
+    fun `remove incorrect amount`() {
+        val account = bank["0005"]
+        if (account != null) {
+            assertFailsWith(IllegalArgumentException::class) {
+                account.removeMoney(-1.0)
+            }
+        } else {
+            assertNotNull(account, "account '0005' cannot be null")
+        }
+    }
+
+    @Test
+    fun `remove too much`() {
+        val account = bank["0005"]
+        if (account != null) {
+            assertFailsWith(NotEnoughMoneyException::class) {
+                try {
+                    account.removeMoney(account.balance() + 10)
+                } catch (e: Exception) {
+                    throw e.cause ?: e
+                }
+            }
+        } else {
+            assertNotNull(account, "account '0005' cannot be null")
+        }
+    }
+
+    @Test
     fun `increment account by 1 for 100 times concurrently`() {
         val executorService = Executors.newFixedThreadPool(10)
         val account = bank["0000"]

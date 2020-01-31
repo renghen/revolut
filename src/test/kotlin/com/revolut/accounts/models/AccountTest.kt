7f50354c2eacd30c7b7ee1f4294c0197d04b6eb8
renghen <renghen@yahoo.com>
2020-01-31 17:44:34 +0400

    2 more bank test added, 1 account conccurent test for mass increment

--- src/test/kotlin/com/revolut/accounts/models/AccountTest.kt
+++ src/test/kotlin/com/revolut/accounts/models/AccountTest.kt
@@ -8,10 +8,27 @@ import kotlin.test.*
 
 class AccountTest {
 
-    companion object{
+    companion object {
         val bank = BankTest.`concurrent creation of N Accounts`(100)
     }
 
-    
+    @Test
+    fun `increment account by 1 for 100 times concurrently`() {
+        val executorService = Executors.newFixedThreadPool(10)
+        val account = bank["0000"]
+
+        if (account != null) {
+            val futures = (1..100).map {
+                CompletableFuture.supplyAsync(Supplier {
+                    account.addMoney(1.0)
+                }, executorService)
+            }.toTypedArray()
+
+            CompletableFuture.allOf(*futures).get()
+            assertEquals(200.00, account.balance())
+        } else {
+            assertNotNull(account, "account '0000' cannot be null")
+        }
+    }
 
 }
--- src/test/kotlin/com/revolut/accounts/models/BankTest.kt
+++ src/test/kotlin/com/revolut/accounts/models/BankTest.kt
@@ -1,6 +1,7 @@
 package com.revolut.accounts.models
 
 import org.junit.Test
+import java.lang.Exception
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
 import java.util.function.Supplier
@@ -9,7 +10,7 @@ import kotlin.test.*
 
 class BankTest {
 
-    companion object{
+    companion object {
         fun `concurrent creation of N Accounts`(n: Int): Bank {
             val bank = Bank("abc")
             val executorService = Executors.newFixedThreadPool(10)
@@ -41,8 +42,6 @@ class BankTest {
         assertEquals(900, bank.getAccountAvailable())
     }
 
-
-
     @Test
     fun `100 account creation concurrent`() {
         val bank = `concurrent creation of N Accounts`(100)
@@ -56,12 +55,34 @@ class BankTest {
         var i = 0
         accounts.forEach {
             val expectedAccountNumber = i.toString().padStart(4, '0')
-            i +=1
+            i += 1
             assertEquals(expectedAccountNumber, it.accountNumber)
             assertEquals(100.00, it.balance())
         }
     }
 
+    @Test
+    fun `1001 account creation concurrent should produce an exception`() {
+        assertFailsWith(AccountFullException::class) {
+            try {
+                `concurrent creation of N Accounts`(1001)
+            } catch (e: Exception) {
+                println(e.cause)
+                throw e.cause?.cause ?: e
+            }
+        }
+    }
+
+    @Test
+    fun `1001 account creation concurrent should still have 1000 accounts,even with exception`() {
+        val bank: Bank = `concurrent creation of N Accounts`(1000)
+        try {
+            bank.createAccount(AccountDetails("1001"), 100.0)
+        } catch (e: Exception) {
+            println(e.cause)
+        }
+        assertEquals(1000, bank.getAccounts().size)
+    }
 }
 
 
